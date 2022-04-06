48a0be92ac589d41aea248422c86db7ba8be6af5
Jaw <tuyweber@gmail.com>
2022-04-06 01:26:44 -0300

    refinado o comando

--- Exercise1_myway.py
+++ Exercise1_myway.py
@@ -18,10 +18,18 @@ def create_person() -> Person:
     person_name = str(input("Please inform the persons name: "))
     person_age = int(input("Please inform the persons age: "))
 
-    person = Person(person_name, person_age)
+    person = Person(name=person_name,age=person_age)
     return person
 
 person = create_person()
+person_list = []
+response = "S"
 
-print(f"{person.name.capitalize()} youre going to be a hundred years old in the year {person.year_of_hundred_years_old()}")
-
+while response in "Ss":
+    print(f"{person.name.capitalize()} youre going to be a hundred years old in the year {person.year_of_hundred_years_old()}")
+    person_info_tuple = (person.name.capitalize(), person.year_of_hundred_years_old())
+    person_list.append(person_info_tuple)
+    response = str(input("Do you whanna continue[S/N]: "))
+    if response in "Nn":
+        break
+    person = create_person()
